eb462c3ef887f37bf4acee1c3951a4a3e85a6c0b
anavalles175 <anavalles175@gmail.com>
2025-03-09 12:13:47 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,6 @@
+// This is a comment 
+#include <stdio.h> // This is another comment 
+int main(void) { // This is yet another comment
+printf("Hello World\n"); // This is a line that prints "Hello World" on the screen 
+return 0; // This exits the program with an error code of 0, indicating success
+}
