0115d875b1a1e6baf067d2c3b416400201fb1fa5
anavalles175 <anavalles175@gmail.com>
2025-03-12 15:28:44 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,11 @@
+
+// C program to print numbers from 1 to 10
+#include <stdio.h>
+
+int main() {
+    // for loop to iterate from 1 to 10
+    for (int i = 1; i <= 10; ++i) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
