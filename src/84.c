07ec742b845758f92f190d344db25c75e1fe315d
anavalles175 <anavalles175@gmail.com>
2025-04-09 21:31:09 +0000

    Add 84.c

---
+++ src/84.c
@@ -0,0 +1,12 @@
+// C Code Example: Simple Addition Function
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int result = add(2, 3);
+    printf("The sum is: %d\n", result); // Output: The sum is: 5
+    return 0;
+}
