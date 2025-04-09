0f0a87d8962f7e40696dc74d33aeae1396ad89fb
anavalles175 <anavalles175@gmail.com>
2025-04-09 20:45:17 +0000

    Add 45.c

---
+++ src/45.c
@@ -0,0 +1,11 @@
+// C code for a simple counter
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i < 5) {
+        printf("Counter value: %d\n", i);
+        i++;
+    }
+    return 0;
+}
