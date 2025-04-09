3230c591c115f74d852a8a96c9b30036f314435d
anavalles175 <anavalles175@gmail.com>
2025-04-09 20:53:56 +0000

    Add 53.c

---
+++ src/53.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int i;
+
+    printf("This is a simple C program.\n");
+    printf("It demonstrates basic operations and loops.");
+    printf("You can run it in most modern Unix-like operating systems.");
+
+    for (i = 0; i < 5; i++) {
+        printf("Looping the loop. Number %d.\n", i);
+    }
+
+    // Add more code here if needed
+}
