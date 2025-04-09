46f46ff4c5397dce517a30611a08fe04e673e8e5
anavalles175 <anavalles175@gmail.com>
2025-04-09 20:56:04 +0000

    Add 55.c

---
+++ src/55.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("Hello, World!\n");
+    
+    for (i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            continue;
+        }
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
