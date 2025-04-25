a54f7e7b70cf4ce6358e290f8265cb3733d28335
anavalles175 <anavalles175@gmail.com>
2025-04-25 07:22:41 +0000

    Add 100.c

---
+++ src/100.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    do {
+        printf("Do something\n");
+        ++i;
+    } while (i < 5);
+    printf("The code is done.\n");
+    return 0;
+}
