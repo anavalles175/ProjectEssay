d33195efd2e8d31c157b750a7ba3d4bf2c86c389
anavalles175 <anavalles175@gmail.com>
2025-05-18 22:44:42 +0000

    Add 103.c

---
+++ src/103.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        printf("Number 1 is greater than Number 2.\n");
+    } else {
+        printf("Number 1 is not greater than Number 2.\n");
+    }
+
+    return 0;
+}
