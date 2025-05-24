206d3c5f78503bd11bd587f93aef499dcfc1f837
anavalles175 <anavalles175@gmail.com>
2025-05-24 18:20:28 +0000

    Add 107.c

---
+++ src/107.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result = num1 + num2;
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+    return 0;
+}
